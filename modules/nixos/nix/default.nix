5114686d8291018ee4cc612888db0df92fee94d1
lcx12901 <wktl1991504424@gmail.com>
2024-05-19 23:17:30 +0800

    WIP: nix module

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -1,1 +1,26 @@
-{}
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkDefault mkIf mkForce;
+
+  cfg = option.wktlNix.nix;
+in {
+  config = mkIf cfg.enable {
+    environment = {
+      etc = with inputs; {
+        # set channels (backwards compatibility)
+        "nix/flake-channels/system".source = self;
+        "nix/flake-channels/nixpkgs".source = nixpkgs;
+        "nix/flake-channels/home-manager".source = home-manager;
+
+        # preserve current flake in /etc
+        "nixos/flake".source = self;
+      };
+
+      systemPackages = [pkgs.gitMinimal];
+    };
+  };
+}
