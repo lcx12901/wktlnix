37af8fffa4f2e66f7435a084e14d77ff72e75e00
lcx12901 <wktl1991504424@gmail.com>
2024-05-20 23:02:55 +0800

    fix: module option not in home

--- systems/x86_64-linux/megumin/default.nix
+++ systems/x86_64-linux/megumin/default.nix
@@ -1,6 +1,12 @@
 {
   imports = [./hardware.nix];
 
+  wktlNix = {
+    archetypes = {
+      wsl = true;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
