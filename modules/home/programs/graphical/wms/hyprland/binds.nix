69d524ee6f3b74a6d7159fddaccc44e9e51782c4
lcx12901 <wktl1991504424@gmail.com>
2024-05-26 19:28:56 +0800

    WIP: hyprland

---
+++ modules/home/programs/graphical/wms/hyprland/binds.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf getExe;
+
+  cfg = config.${namespace}.programs.graphical.wms.hyprland;
+in {
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      settings = {
+        bind = [
+          # █░░░█▀█░█░█░█▀█░█▀▀░█░█░█▀▀░█▀▄░█▀▀
+          # █░░░█▀█░█░█░█░█░█░░░█▀█░█▀▀░█▀▄░▀▀█
+          # ▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+          # ░█▀█░█▀█░█▀█░█▀▀
+          # ░█▀█░█▀▀░█▀▀░▀▀█
+          # ░▀░▀░▀░░░▀░░░▀▀▀
+        ];
+      };
+    };
+  };
+}
