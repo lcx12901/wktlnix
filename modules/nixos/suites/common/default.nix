ffd7860a03e2a75635a37ee30621d4613c783593
linchengxu <linchengxu@z9yun.com>
2024-05-27 17:15:33 +0800

    fix

--- modules/nixos/suites/common/default.nix
+++ modules/nixos/suites/common/default.nix
@@ -12,6 +12,10 @@ let
   cfg = config.${namespace}.suites.common;
 in
 {
+  options.${namespace}.suites.common = {
+    enable = mkBoolOpt false "Whether or not to enable common configuration.";
+  };
+
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       curl
