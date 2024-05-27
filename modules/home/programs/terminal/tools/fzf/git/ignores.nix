c6f12d248a3878f680e6530b7a05ea8a8e37a95c
Chengxu Lin <wktl1991504424@gmail.com>
2024-05-27 22:45:44 +0800

    feat: add git

---
+++ modules/home/programs/terminal/tools/fzf/git/ignores.nix
@@ -0,0 +1,43 @@
+{
+  ignores = [
+    # General
+    ".cache/"
+    "tmp/"
+    "*.tmp"
+    "log/"
+    ".DS_Store"
+    "Desktop.ini"
+    "Thumbs.db"
+    "._*"
+    ".Spotlight-V100"
+    ".Trashes"
+
+    # IDE
+    "*.swp"
+    ".idea/"
+    ".~lock*"
+
+    # C
+    ".tags"
+    "tags"
+    "*~"
+    "*.o"
+    "*.so"
+    "*.cmake"
+    "CMakeCache.txt"
+    "CMakeFiles/"
+    "cmake-build-debug/"
+    "compile_commands.json"
+    ".ccls*"
+    "*.out"
+
+    # Nix
+    "result"
+    "result-*"
+    ".direnv/"
+
+    # Node
+    "node_modules/"
+    "dist/"
+  ];
+}
