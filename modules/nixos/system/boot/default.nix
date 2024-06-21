991d0de1883c6fa2d49fad9e4798d699f17e9ed3
lcx12901 <wktl1991504424@gmail.com>
2024-06-22 00:37:03 +0800

    update: set watchdog

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -41,6 +41,11 @@ in {
           "vt.global_cursor_default=0"
         ];
 
+      kernel.sysctl = {
+        "kernel.sysrq" = 1;
+        "vm.swappiness" = 10;
+      };
+
       loader = {
         efi = {
           canTouchEfiVariables = true;
@@ -70,6 +75,33 @@ in {
     services.fwupd = {
       enable = true;
       daemonSettings.EspLocation = config.boot.loader.efi.efiSysMountPoint;
+    };
+
+    systemd = {
+      # given that our systems are headless, emergency mode is useless.
+      # we prefer the system to attempt to continue booting so
+      # that we can hopefully still access it remotely.
+      enableEmergencyMode = false;
+
+      # For more detail, see:
+      #   https://0pointer.de/blog/projects/watchdog.html
+      watchdog = {
+        # systemd will send a signal to the hardware watchdog at half
+        # the interval defined here, so every 10s.
+        # If the hardware watchdog does not get a signal for 20s,
+        # it will forcefully reboot the system.
+        runtimeTime = "20s";
+        # Forcefully reboot if the final stage of the reboot
+        # hangs without progress for more than 30s.
+        # For more info, see:
+        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+        rebootTime = "30s";
+      };
+
+      sleep.extraConfig = ''
+        AllowSuspend=no
+        AllowHibernation=no
+      '';
     };
   };
 }
