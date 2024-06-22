d6ec5c92981aa326698cfdcfb243457a0bd39ee3
lcx12901 <wktl1991504424@gmail.com>
2024-06-22 22:24:03 +0800

    Add initial configuration for kanroji

---
+++ homes/x86_64-linux/wktl@kanroji/default.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) enabled;
+in {
+  wktlnix = {
+    user = {
+      enable = true;
+      inherit (config.snowfallorg.user) name;
+    };
+
+    system.xdg = enabled;
+
+    programs = {
+      graphical = {
+        wms.hyprland = enabled;
+        browsers.firefox = {
+          gpuAcceleration = true;
+          hardwareDecoding = true;
+        };
+        editors.vscode = enabled;
+        launchers.rofi = enabled;
+        video = enabled;
+      };
+      terminal = {
+        emulators.kitty = enabled;
+        media.spicetify = enabled;
+        tools = {
+          git = enabled;
+          direnv = enabled;
+        };
+      };
+    };
+
+    theme.gtk = enabled;
+
+    scenes = {
+      daily = enabled;
+      business = enabled;
+      development = enabled;
+    };
+  };
+
+  home.stateVersion = "24.05";
+}
---
+++ modules/nixos/services/openssh/default.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt mkOpt;
+  inherit (lib.types) port;
+
+  cfg = config.${namespace}.services.openssh;
+in {
+  options.${namespace}.services.openssh = {
+    enable = mkBoolOpt false "Whether or not to configure OpenSSH support.";
+    port = mkOpt port 2233 "The port to listen on (in addition to 22).";
+  };
+
+  config = mkIf cfg.enable {
+    services.openssh = {
+      enable = true;
+    };
+  };
+}
