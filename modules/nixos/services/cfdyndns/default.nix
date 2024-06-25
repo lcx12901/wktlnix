b468dc6226f816204c26ca45a08790a59bad8d21
lcx12901 <wktl1991504424@gmail.com>
2024-06-25 23:23:47 +0800

    update

--- modules/nixos/services/cfdyndns/default.nix
+++ modules/nixos/services/cfdyndns/default.nix
@@ -5,23 +5,24 @@
   ...
 }: let
   inherit (lib) mkIf;
-  inherit (lib.types) str;
-  inherit (lib.${namespace}) mkBoolOpt mkOpt;
+  inherit (lib.${namespace}) mkBoolOpt;
 
   cfg = config.${namespace}.services.cfdyndns;
 in {
   options.${namespace}.services.cfdyndns = {
     enable = mkBoolOpt false "Whether or not to enable Cloudflare Dynamic DNS Client.";
-    email = mkOpt str "wktl1991504424@gmail.com" "The email address to use to authenticate to CloudFlare.";
   };
 
   config = mkIf cfg.enable {
-    services.cfdyndns = {
-      inherit (cfg) enable email;
+    services.cloudflare-dyndns = {
+      inherit (cfg) enable;
 
       apiTokenFile = config.age.secrets."cfAPI.conf".path;
 
-      records = ["${config.networking.hostName}.lincx.top"];
+      domains = ["${config.networking.hostName}.lincx.top"];
+
+      ipv4 = false;
+      ipv6 = true;
     };
   };
 }
