0fa2683410506fe0662060853394d377d355bfcd
lcx12901 <wktl1991504424@gmail.com>
2024-07-04 20:26:04 +0800

    WIP: catppuccin theme init

---
+++ modules/home/theme/catppuccin/default.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption mkOption types;
+
+  catppuccinAccents = [
+    "rosewater"
+    "flamingo"
+    "pink"
+    "mauve"
+    "red"
+    "maroon"
+    "peach"
+    "yellow"
+    "green"
+    "teal"
+    "sky"
+    "sapphire"
+    "blue"
+    "lavender"
+  ];
+
+  catppuccinFlavors = [
+    "latte"
+    "frappe"
+    "macchiato"
+    "mocha"
+  ];
+
+  cfg = config.${namespace}.theme.catppuccin;
+in {
+  options.${namespace}.theme.catppuccin = {
+    enable = mkEnableOption "Enable catppuccin theme for applications.";
+
+    accent = mkOption {
+      type = types.enum catppuccinAccents;
+      default = "lavender";
+      description = ''
+        An optional theme accent.
+      '';
+    };
+
+    flavor = mkOption {
+      type = types.enum catppuccinFlavors;
+      default = "macchiato";
+      description = ''
+        An optional theme flavor.
+      '';
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.catppuccin.override {
+        inherit (cfg) accent;
+        variant = cfg.flavor;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    catppuccin = {
+      # NOTE: Need some customization and merging of configuration files so cant just enable all
+      enable = false;
+
+      inherit (cfg) accent flavor;
+    };
+  };
+}
