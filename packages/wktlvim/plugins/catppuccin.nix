32053790d9facc966d3a2f880361ea15fd1e08cf
lcx12901 <wktl1991504424@gmail.com>
2024-08-19 00:26:30 +0800

    feat: nixnvim packages

---
+++ packages/wktlvim/plugins/catppuccin.nix
@@ -0,0 +1,12 @@
+_: {
+  colorschemes.catppuccin.settings = {
+    dim_inactive = {
+      enabled = false;
+      percentage = 0.25;
+    };
+
+    flavour = "macchiato";
+
+    transparent_background = true;
+  };
+}
---
+++ packages/wktlvim/plugins/gitsigns.nix
@@ -0,0 +1,22 @@
+{
+  plugins.gitsigns = {
+    enable = true;
+
+    settings = {
+      trouble = true;
+
+      current_line_blame = true;
+
+      current_line_blame_opts = {
+        delay = 500;
+
+        ignore_blank_lines = true;
+        ignore_whitespace = true;
+        virt_text = true;
+        virt_text_pos = "eol";
+      };
+
+      signColumn = false;
+    };
+  };
+}
