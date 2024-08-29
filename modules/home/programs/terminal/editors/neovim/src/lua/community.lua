9ed407d1bc58c44d6fc68ef6603c01dfd935f422
lcx12901 <wktl1991504424@gmail.com>
2024-08-30 01:21:13 +0800

    update: astronvim config

--- modules/home/programs/terminal/editors/neovim/src/lua/community.lua
+++ modules/home/programs/terminal/editors/neovim/src/lua/community.lua
@@ -5,15 +5,34 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Motion
+  { import = "astrocommunity.motion.mini-surround" },
+  -- https://github.com/echasnovski/mini.ai
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.pack.lua" },
 
   -- Language Support
   ---- Frontend & NodeJS
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.vue" },
+  -- { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.prisma" },
+  ---- Configuration Language
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  ---- Backend / System
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  ---- Operation & Cloud Native
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
 
   -- colorscheme
   { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.recipes.neovide" },
 }
---
+++ modules/home/programs/terminal/editors/neovim/src/lua/plugins/none-ls.lua
@@ -0,0 +1,46 @@
+-- Customize None-ls sources
+
+---@type LazySpec
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local null_ls = require "null-ls"
+    local code_actions = null_ls.builtins.code_actions
+    local diagnostics = null_ls.builtins.diagnostics
+    local formatting = null_ls.builtins.formatting
+    local hover = null_ls.builtins.hover
+    local completion = null_ls.builtins.completion
+
+    -- Check supported formatters and linters
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      -- Common Code Actions
+      code_actions.gitsigns,
+      -- common refactoring actions based off the Refactoring book by Martin Fowler
+      code_actions.refactoring,
+      code_actions.proselint, -- English prose linter
+      code_actions.statix, -- Lints and suggestions for Nix.
+
+      -- Diagnostic
+      diagnostics.actionlint, -- GitHub Actions workflow syntax checking
+      diagnostics.buf, -- check text in current buffer
+      diagnostics.checkmake, -- check Makefiles
+      diagnostics.deadnix, -- Scan Nix files for dead code.
+
+      -- Formatting
+      diagnostics.hadolint, -- Dockerfile linter
+      formatting.black, -- Python formatter
+      formatting.shfmt, -- Shell formatter
+      formatting.stylua, -- Lua formatter
+      formatting.alejandra, -- Nix formatter
+      formatting.sqlfluff.with { -- SQL formatter
+        extra_args = { "--dialect", "postgres" }, -- change to your dialect
+      },
+      formatting.nginx_beautifier, -- Nginx formatter
+    }
+
+    return config -- return final config table
+  end,
+}
