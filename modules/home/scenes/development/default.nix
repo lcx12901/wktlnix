ad2e8f0db41f7f92a48e6339a874d207de759952
lcx12901 <wktl1991504424@gmail.com>
2024-09-02 00:33:36 +0800

    feat: add bun

--- modules/home/scenes/development/default.nix
+++ modules/home/scenes/development/default.nix
@@ -1,4 +1,5 @@
 {
+  osConfig,
   config,
   lib,
   pkgs,
@@ -10,6 +11,8 @@
 
   homeDirectory = config.${namespace}.user.home;
 
+  persist = osConfig.${namespace}.system.persist.enable;
+
   cfg = config.${namespace}.scenes.development;
 in {
   options.${namespace}.scenes.development = {
@@ -17,11 +20,21 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      nodejs_20
-      yarn
-      pnpm
-    ];
+    home = {
+      packages = with pkgs; [
+        nodejs_20
+        yarn
+        pnpm
+        bun
+      ];
+
+      persistence = mkIf persist {
+        "/persist/home/${config.${namespace}.user.name}" = {
+          allowOther = true;
+          directories = [".bun"];
+        };
+      };
+    };
 
     nixpkgs.config = {
       programs.npm.npmrc = ''
@@ -29,15 +42,42 @@ in {
       '';
     };
 
-    xdg.configFile."pnpm/rc".text = ''
-      cache-dir=${homeDirectory}/Coding/.pnpm-store/cache
-      global-bin-dir=${homeDirectory}/Coding/.pnpm-store
-      state-dir=${homeDirectory}/Coding/.pnpm-store/state
-      global-dir=${homeDirectory}/Coding/.pnpm-store/global
-    '';
+    xdg.configFile = {
+      "pnpm/rc".text = ''
+        cache-dir=${homeDirectory}/Coding/.pnpm-store/cache
+        global-bin-dir=${homeDirectory}/Coding/.pnpm-store
+        state-dir=${homeDirectory}/Coding/.pnpm-store/state
+        global-dir=${homeDirectory}/Coding/.pnpm-store/global
+      '';
+
+      ".bunfig.toml".text = ''
+        [install]
+        # where `bun install --global` installs packages
+        globalDir = "~/.bun/install/global"
+
+        # where globally-installed package bins are linked
+        globalBinDir = "~/.bun/bin"
+
+        [install.cache]
+        # the directory to use for the cache
+        dir = "~/.bun/install/cache"
+
+        # when true, don't load from the global cache.
+        # Bun may still write to node_modules/.cache
+        disable = false
+
+        # when true, always resolve the latest versions from the registry
+        disableManifest = false
+      '';
+    };
 
     wktlnix = {
-      programs.terminal.tools.lazygit = enabled;
+      programs = {
+        terminal = {
+          tools.lazygit = enabled;
+          editors.neovim = enabled;
+        };
+      };
     };
   };
 }
