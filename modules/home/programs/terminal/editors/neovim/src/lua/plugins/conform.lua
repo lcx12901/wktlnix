e6b12df503fa1a1dc0356cafcd6335d25fa31f1b
lcx12901 <wktl1991504424@gmail.com>
2024-09-10 00:35:27 +0800

    update: lsp volar and eslint config

--- modules/home/programs/terminal/editors/neovim/src/lua/plugins/conform.lua
+++ modules/home/programs/terminal/editors/neovim/src/lua/plugins/conform.lua
@@ -5,24 +5,7 @@ return {
     require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua" },
-        -- typescript = { "eslint_d" },
-        -- javascript = { "eslint_d" },
-        -- typescriptreact = { "eslint_d" },
-        -- vue = { "eslint_d" },
-        -- css = { "eslint_d" },
-        -- scss = { "eslint_d" },
-        -- less = { "eslint_d" },
-        -- json = { "eslint_d" },
-        -- jsonc = { "eslint_d" },
-        -- html = { "eslint_d" },
         nix = { "alejandra" },
-      },
-      formatters = {
-        -- eslint_d = {
-        --   command = "eslint_d",
-        --   args = { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
-        --   cwd = require("conform.util").root_file { "package.json" },
-        -- },
       },
     }
 
--- modules/home/programs/terminal/editors/neovim/src/lua/plugins/disabled.lua
+++ modules/home/programs/terminal/editors/neovim/src/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
+  { "nvimtools/none-ls.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "jay-babu/mason-null-ls.nvim", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
