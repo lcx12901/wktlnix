047ada21aa5e4494b7f7a7815b0f313b5bb8dfd0
lcx12901 <wktl1991504424@gmail.com>
2024-09-16 04:03:29 +0800

    feat: add eza

---
+++ modules/home/programs/terminal/tools/eza/default.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.programs.terminal.tools.eza;
+in {
+  options.${namespace}.programs.terminal.tools.eza = {
+    enable = mkBoolOpt false "Whether or not to enable eza.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.eza = {
+      enable = true;
+      package = pkgs.eza;
+
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+
+      extraOptions = [
+        "--group-directories-first"
+        "--header"
+      ];
+
+      git = true;
+      icons = true;
+    };
+
+    home.shellAliases = {
+      la = lib.mkForce "${lib.getExe config.programs.eza.package} -lah --tree";
+      tree = lib.mkForce "${lib.getExe config.programs.eza.package} --tree --icons=always";
+    };
+  };
+}
