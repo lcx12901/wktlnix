834197ccdc6861d9ffd07fc726e7030b29485b05
lcx12901 <wktl1991504424@gmail.com>
2024-10-26 16:35:50 +0800

    chore: add trustCa for ryomori tls

---
+++ modules/nixos/security/pki/trustCa/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.security.pki.trustCa;
+in {
+  options.${namespace}.security.pki.trustCa = {
+    enable = mkBoolOpt false "trustCa";
+  };
+
+  config = lib.mkIf cfg.enable {
+    security.pki.certificates = [
+      (builtins.readFile ./ryomori.cer)
+    ];
+  };
+}
