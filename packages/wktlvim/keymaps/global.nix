253846b562358f3a11bafe14600bbbde74a86b91
lcx12901 <wktl1991504424@gmail.com>
2024-11-17 18:34:11 +0800

    chore(packages/wktlvim): add some keymap

--- packages/wktlvim/keymaps/global.nix
+++ packages/wktlvim/keymaps/global.nix
@@ -31,7 +31,100 @@
             "<esc>" = {
               action = "<cmd>noh<CR>";
             };
+            "j" = {
+              action = "v:count == 0 ? 'gj' : 'j'";
+              options = {
+                desc = "Move cursor down";
+                expr = true;
+              };
+            };
+            "k" = {
+              action = "v:count == 0 ? 'gk' : 'k'";
+              options = {
+                desc = "Move cursor up";
+                expr = true;
+              };
+            };
+          };
+
+      visual =
+        lib.mapAttrsToList
+          (
+            key:
+            { action, ... }@attrs:
+            {
+              mode = "v";
+              inherit action key;
+              options = attrs.options or { };
+            }
+          )
+          {
+            # Better indenting
+            "<S-Tab>" = {
+              action = "<gv";
+              options = {
+                desc = "Unindent line";
+              };
+            };
+            "<" = {
+              action = "<gv";
+              options = {
+                desc = "Unindent line";
+              };
+            };
+            "<Tab>" = {
+              action = ">gv";
+              options = {
+                desc = "Indent line";
+              };
+            };
+            ">" = {
+              action = ">gv";
+              options = {
+                desc = "Indent line";
+              };
+            };
+
+            # Move selected line/block in visual mode
+            "K" = {
+              action = "<cmd>m '<-2<CR>gv=gv<cr>";
+            };
+            "J" = {
+              action = "<cmd>m '>+1<CR>gv=gv<cr>";
+            };
+
+            # Backspace delete in visual
+            "<BS>" = {
+              action = "x";
+            };
+          };
+
+      insert =
+        lib.mapAttrsToList
+          (
+            key:
+            { action, ... }@attrs:
+            {
+              mode = "i";
+              inherit action key;
+              options = attrs.options or { };
+            }
+          )
+          {
+            # Move selected line/block in insert mode
+            "<C-k>" = {
+              action = "<C-o>gk";
+            };
+            "<C-h>" = {
+              action = "<Left>";
+            };
+            "<C-l>" = {
+              action = "<Right>";
+            };
+            "<C-j>" = {
+              action = "<C-o>gj";
+            };
           };
     in
-    helpers.keymaps.mkKeymaps { options.silent = true; } normal;
+    helpers.keymaps.mkKeymaps { options.silent = true; } normal ++ visual ++ insert;
 }
