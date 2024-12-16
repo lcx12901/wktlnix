b11c2b0563d253d3bd4af3abb74f524fb2498bc0
linchengxu <linchengxu@z9yun.com>
2024-12-16 19:20:06 +0800

    packages/wktlvim: make neovide can use ctrl+c/v

--- packages/wktlvim/plugins/neovide.nix
+++ packages/wktlvim/plugins/neovide.nix
@@ -12,6 +12,19 @@
       vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
       vim.o.guifont = "RecMonoCasual Nerd Font,LXGW WenKai Mono:h14"
+
+      -- copy_to_clipboard
+      vim.keymap.set('v', '<C-S-c>', '"+y', { noremap = true, silent = true })
+
+      -- paste_from_clipboard
+      vim.keymap.set('i', '<C-S-v>', '<C-r>+', { noremap = true, silent = true })
+      vim.keymap.set('n', '<C-S-v>', '"+p', { noremap = true, silent = true })
+
+      -- paste_from_clipboard in cmd model
+      vim.keymap.set('c', '<C-S-v>', '<C-R>+', { noremap = true, silent = true })
+
+      -- paste_from_clipboard in terminal model
+      vim.keymap.set('t', '<C-S-v>', '<C-\\><C-n>"+pi', { noremap = true, silent = true })
     end
   '';
 }
