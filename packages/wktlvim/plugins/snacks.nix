da242cd49f6d15d4136daa25194aaf1d2931aea2
lcx12901 <wktl1991504424@gmail.com>
2024-12-20 01:07:01 +0800

    packages/wktlvim: add snacks

---
+++ packages/wktlvim/plugins/snacks.nix
@@ -0,0 +1,124 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  extraConfigLuaPre = lib.mkIf config.plugins.snacks.enable (
+    lib.mkOrder 1 (
+      lib.optionalString config.plugins.snacks.settings.profiler.enabled # Lua
+        ''
+          if vim.env.PROF then
+            local snacks = "${pkgs.vimPlugins.snacks-nvim}"
+            vim.opt.rtp:append(snacks)
+            require("snacks.profiler").startup({
+              startup = {
+                -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+                event = "UIEnter",
+                -- event = "VeryLazy",
+              },
+            })
+          end
+        ''
+    )
+  );
+
+  plugins = {
+    snacks = {
+      enable = true;
+
+      settings = {
+        bigfile = {
+          enabled = true;
+          size = 1024 * 1024; # 1MB
+          setup.__raw = ''
+            function(ctx)
+              ${lib.optionalString config.plugins.indent-blankline.enable ''require("ibl").setup_buffer(0, { enabled = false })''}
+              ${lib.optionalString (lib.hasAttr "indentscope" config.plugins.mini.modules) ''vim.b.miniindentscope_disable = true''}
+              ${lib.optionalString config.plugins.illuminate.enable ''require("illuminate").pause_buf()''}
+
+              -- Disable line numbers and relative line numbers
+              vim.cmd("setlocal nonumber norelativenumber")
+
+              -- Syntax highlighting
+              vim.schedule(function()
+                vim.bo[ctx.buf].syntax = ctx.ft
+              end)
+
+              -- Disable matchparen
+              vim.cmd("let g:loaded_matchparen = 1")
+
+              -- Disable cursor line and column
+              vim.cmd("setlocal nocursorline nocursorcolumn")
+
+              -- Disable folding
+              vim.cmd("setlocal nofoldenable")
+
+              -- Disable sign column
+              vim.cmd("setlocal signcolumn=no")
+
+              -- Disable swap file and undo file
+              vim.cmd("setlocal noswapfile noundofile")
+
+              -- Disable mini animate
+              vim.b.minianimate_disable = true
+            end
+          '';
+        };
+        bufdelete.enabled = true;
+        indent.enabled = true;
+        lazygit.enabled = true;
+        scroll.enabled = true;
+        profiler.enabled = true;
+        statuscolumn = {
+          enabled = true;
+
+          folds = {
+            open = true;
+            git_hl = config.plugins.gitsigns.enable;
+          };
+        };
+      };
+    };
+  };
+
+  keymaps =
+    [
+      (lib.mkIf (config.plugins.snacks.enable && config.plugins.snacks.settings.lazygit.enabled) {
+        mode = "n";
+        key = "<leader>tl";
+        action = "<cmd>lua Snacks.lazygit()<CR>";
+        options = {
+          desc = "Open lazygit";
+        };
+      })
+    ]
+    ++ lib.optionals (config.plugins.snacks.enable && config.plugins.snacks.settings.bufdelete.enabled)
+      [
+        {
+          mode = "n";
+          key = "<leader>c";
+          action = ''<cmd>lua Snacks.bufdelete.delete()<cr>'';
+          options = {
+            desc = "Close buffer";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bc";
+          action = ''<cmd>lua Snacks.bufdelete.other()<cr>'';
+          options = {
+            desc = "Close all buffers but current";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bC";
+          action = ''<cmd>lua Snacks.bufdelete.all()<cr>'';
+          options = {
+            desc = "Close all buffers";
+          };
+        }
+      ];
+}
