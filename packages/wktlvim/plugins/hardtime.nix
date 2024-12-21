2a3adcc4f637f5deeec2b9e4fb315df94b1d3bcb
lcx12901 <wktl1991504424@gmail.com>
2024-12-22 00:28:57 +0800

    packages/wktlvim: add hardtime

---
+++ packages/wktlvim/plugins/hardtime.nix
@@ -0,0 +1,49 @@
+{ config, lib, ... }:
+let
+  cfg = config.plugins.hardtime;
+in
+{
+  globals.disable_hardtime = true;
+
+  plugins = {
+    hardtime = {
+      enable = true;
+
+      settings = {
+        enabled = true;
+        disable_mouse = true;
+      };
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.hardtime.enable [
+      {
+        __unkeyed = "<leader>v";
+        mode = "n";
+        group = "Vim training";
+        icon = "󱛊";
+      }
+    ];
+  };
+
+  keymaps = lib.mkIf cfg.enable [
+    {
+      mode = "n";
+      key = "<leader>vh";
+      action.__raw = ''
+        function ()
+          vim.g.disable_hardtime = not vim.g.disable_hardtime
+          if vim.g.disable_hardtime then
+            require("hardtime").disable()
+          else
+            require("hardtime").enable()
+          end
+          vim.notify(string.format("Hardtime %s", bool2str(not vim.g.disable_hardtime), "info"))
+        end
+      '';
+      options = {
+        desc = "Hardtime toggle";
+        silent = true;
+      };
+    }
+  ];
+}
