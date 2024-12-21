c76455db35c6617d71511ca125ad0a074746277a
lcx12901 <wktl1991504424@gmail.com>
2024-12-22 00:30:22 +0800

    packages/wktlvim: delete comments

--- packages/wktlvim/plugins/neo-tree.nix
+++ packages/wktlvim/plugins/neo-tree.nix
@@ -59,7 +59,6 @@ in
         "<Space>".__raw = "false";
 
         # See extraOptions.commands for details on following keymaps
-        Y = "copy_selector";
         h = "parent_or_close";
         l = "child_or_open";
       };
@@ -151,42 +150,6 @@ in
             end
           end
         '';
-        # Copy various path format of currently focused item
-        # copy_selector.__raw = ''
-        #   function(state)
-        #     local node = state.tree:get_node()
-        #     local filepath = node:get_id()
-        #     local filename = node.name
-        #     local modify = vim.fn.fnamemodify
-        #
-        #     local vals = {
-        #       ["BASENAME"] = modify(filename, ":r"),
-        #       ["EXTENSION"] = modify(filename, ":e"),
-        #       ["FILENAME"] = filename,
-        #       ["PATH (CWD)"] = modify(filepath, ":."),
-        #       ["PATH (HOME)"] = modify(filepath, ":~"),
-        #       ["PATH"] = filepath,
-        #       ["URI"] = vim.uri_from_fname(filepath),
-        #     }
-        #
-        #     local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
-        #     if vim.tbl_isempty(options) then
-        #       astro.notify("No values to copy", vim.log.levels.WARN)
-        #       return
-        #     end
-        #     table.sort(options)
-        #     vim.ui.select(options, {
-        #       prompt = "Choose to copy to clipboard:",
-        #       format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
-        #     }, function(choice)
-        #       local result = vals[choice]
-        #       if result then
-        #         astro.notify(("Copied: `%s`"):format(result))
-        #         vim.fn.setreg("+", result)
-        #       end
-        #     end)
-        #   end
-        # '';
       };
     };
   };
@@ -249,13 +212,6 @@ in
             end
           end
         '';
-      }
-      {
-        desc = "Refresh explorer sources when closing lazygit";
-        event = "TermClose";
-        group = "neotree";
-        pattern = "*lazygit*";
-        callback.__raw = refresh;
       }
       {
         desc = "Refresh explorer sources on focus";
