2f3b920863883699ba091983b17b2dc913b4c7bf
lcx12901 <wktl1991504424@gmail.com>
2025-01-14 01:31:30 +0800

    wms/hyprland: set cursor size

--- modules/home/programs/graphical/wms/hyprland/apps.nix
+++ modules/home/programs/graphical/wms/hyprland/apps.nix
@@ -15,9 +15,8 @@ in
     wayland.windowManager.hyprland = {
       settings = {
         exec-once = [
-          # ░█▀█░█▀█░█▀█░░░█▀▀░▀█▀░█▀█░█▀▄░▀█▀░█░█░█▀█
-          # ░█▀█░█▀▀░█▀▀░░░▀▀█░░█░░█▀█░█▀▄░░█░░█░█░█▀▀
-          # ░▀░▀░▀░░░▀░░░░░▀▀▀░░▀░░▀░▀░▀░▀░░▀░░▀▀▀░▀░░
+          "hyprctl setcursor 22 &"
+
           "fcitx5 -d --replace"
           "swww init && swww img ${inputs.wallpapers}/Hoshino-eye.jpg"
         ];
