a54abb52a1c8bba4c244c330ced87aab17845eb5
lcx12901 <wktl1991504424@gmail.com>
2025-01-19 23:45:01 +0800

    home/niri: update focus-ring

--- modules/home/programs/graphical/wms/niri/default.nix
+++ modules/home/programs/graphical/wms/niri/default.nix
@@ -84,17 +84,28 @@ in
 
         focus-ring = {
           enable = true;
-          width = 4;
+          width = 3;
           active.color = "#ca9ee6";
-          inactive.color = "#505050";
+          inactive.color = "#babbf1";
         };
 
         border = {
           enable = false;
           width = 4;
-          active.color = "#ffc87f";
-          inactive.color = "#505050";
+          active.color = "#e78284";
+          inactive.color = "#babbf1";
         };
+
+        # shadow = {
+        #   enable = true;
+        #   softness = 30;
+        #   spread = 5;
+        #   offset = {
+        #     x = 1;
+        #     y = 5;
+        #   };
+        #   color = "#ea999c";
+        # };
       };
 
       prefer-no-csd = true;
@@ -122,7 +133,44 @@ in
 
       animations = {
         enable = true;
-        # all default
+        # https://github.com/sodiboo/system/blob/main/niri.mod.nix#L312
+        shaders.window-resize = ''
+          vec4 resize_color(vec3 coords_curr_geo, vec3 size_curr_geo) {
+            vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
+
+            vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
+            vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
+
+            // We can crop if the current window size is smaller than the next window
+            // size. One way to tell is by comparing to 1.0 the X and Y scaling
+            // coefficients in the current-to-next transformation matrix.
+            bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
+            bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
+
+            vec3 coords = coords_stretch;
+            if (can_crop_by_x)
+              coords.x = coords_crop.x;
+            if (can_crop_by_y)
+              coords.y = coords_crop.y;
+
+            vec4 color = texture2D(niri_tex_next, coords.st);
+
+            // However, when we crop, we also want to crop out anything outside the
+            // current geometry. This is because the area of the shader is unspecified
+            // and usually bigger than the current geometry, so if we don't fill pixels
+            // outside with transparency, the texture will leak out.
+            //
+            // When stretching, this is not an issue because the area outside will
+            // correspond to client-side decoration shadows, which are already supposed
+            // to be outside.
+            if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
+              color = vec4(0.0);
+            if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
+              color = vec4(0.0);
+
+            return color;
+          }
+        '';
       };
 
       binds =
