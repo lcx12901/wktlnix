361446fece3bdb28a1b781dba6754203d1126c16
lcx12901 <wktl1991504424@gmail.com>
2025-01-24 00:40:56 +0800

    add host for M600

---
+++ systems/x86_64-linux/hiyori/default.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) enabled;
+in
+{
+  imports = [ ./hardware.nix ];
+
+  wktlnix = {
+    system = {
+      boot = enabled;
+      disko = {
+        enable = true;
+        device = "/dev/nvme0n1";
+      };
+      fonts = enabled;
+      locale = {
+        enable = true;
+      };
+      time = enabled;
+      persist = enabled;
+      networking = enabled;
+    };
+
+    hardware = {
+      audio = enabled;
+      graphics = enabled;
+      cpu.amd = enabled;
+      gpu.amd = enabled;
+      btrfs = enabled;
+      bluetooth = enabled;
+    };
+
+    programs = {
+      graphical = {
+        wms = {
+          niri = enabled;
+        };
+        games = {
+          steam = enabled;
+          gamemode = enabled;
+        };
+        addons.xdg-portal = enabled;
+        file-managers.nautilus = enabled;
+      };
+      terminal.tools.nix-ld = enabled;
+    };
+
+    suites = {
+      common = enabled;
+      wlroots = enabled;
+    };
+
+    services = {
+      mihomo = enabled;
+      openssh = enabled;
+    };
+
+    security.pki.trustCa = enabled;
+    virtualisation.waydroid = enabled;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
