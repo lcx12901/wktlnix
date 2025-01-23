60c51821f5f3da831a4f563653e4394555a3795b
lcx12901 <wktl1991504424@gmail.com>
2025-01-24 00:40:56 +0800

    add host for M600

---
+++ homes/x86_64-linux/wktl@hiyori/default.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) enabled;
+in
+{
+  wktlnix = {
+    user = {
+      enable = true;
+      inherit (config.snowfallorg.user) name;
+    };
+
+    system.xdg = enabled;
+
+    programs = {
+      graphical = {
+        wms = {
+          niri = enabled;
+        };
+        browsers.firefox = {
+          enable = true;
+          gpuAcceleration = true;
+          hardwareDecoding = true;
+        };
+      };
+    };
+
+    scenes = {
+      daily = enabled;
+      business = enabled;
+      development = enabled;
+    };
+  };
+
+  home.stateVersion = "24.05";
+}
---
+++ systems/x86_64-linux/hiyori/hardware.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ehci_pci"
+        "ahci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "sr_mod"
+      ];
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableRedistributableFirmware = true;
+}
