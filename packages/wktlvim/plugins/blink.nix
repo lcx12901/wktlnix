43d87ae7363c4f2df199949b48d179aa359bc368
lcx12901 <wktl1991504424@gmail.com>
2025-02-07 02:20:57 +0800

    wktlvim: plugin tweaks

--- packages/wktlvim/plugins/blink.nix
+++ packages/wktlvim/plugins/blink.nix
@@ -10,7 +10,7 @@
   extraPlugins = lib.mkIf config.plugins.blink-cmp.enable (
     with pkgs;
     [
-      vimPlugins.blink-compat
+      wordnet
     ]
   );
 
@@ -20,13 +20,10 @@
         enable = true;
         package = inputs.blink-cmp.packages.${system}.default;
 
-        luaConfig.pre = ''
-          require('blink.compat').setup({debug = false, impersonate_nvim_cmp = true})
-        '';
+        lazyLoad.settings.event = "InsertEnter";
 
         settings = {
           completion = {
-            accept.auto_brackets.enabled = true;
             ghost_text.enabled = true;
             documentation = {
               auto_show = true;
@@ -46,6 +43,27 @@
                   }
                   { __unkeyed-1 = "source_name"; }
                 ];
+                components = {
+                  kind_icon = {
+                    ellipsis = false;
+                    text.__raw = ''
+                      function(ctx)
+                        local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                        -- Check for both nil and the default fallback icon
+                        if not kind_icon or kind_icon == '󰞋' then
+                          -- Use our configured kind_icons
+                          return require('blink.cmp.config').appearance.kind_icons[ctx.kind] or ""
+                        end
+                        return kind_icon
+                      end,
+                      -- Optionally, you may also use the highlights from mini.icons
+                      highlight = function(ctx)
+                        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return hl
+                      end
+                    '';
+                  };
+                };
               };
             };
           };
@@ -83,83 +101,84 @@
           };
           snippets.preset = "mini_snippets";
           sources = {
-            default = [
-              "buffer"
-              "codeium"
-              "lsp"
-              "path"
-              "snippets"
-              "emoji"
-              "git"
-              #"npm"
-              "spell"
-              #"treesitter"
-              # "zsh"
-            ];
-            providers = {
-              calc = {
-                name = "calc";
-                module = "blink.compat.source";
-                score_offset = 2;
+            default =
+              [
+                "buffer"
+                "lsp"
+                "path"
+                "snippets"
+                "emoji"
+                # "git"
+                "dictionary"
+                "npm"
+                "calc"
+                "spell"
+                #"treesitter"
+                # "zsh"
+              ]
+              ++ lib.optionals config.plugins.codeium-nvim.enable [
+                "codeium"
+              ];
+            providers =
+              {
+                # BUILT-IN SOURCES
+                lsp.score_offset = 4;
+                dictionary = {
+                  name = "Dict";
+                  module = "blink-cmp-dictionary";
+                  min_keyword_length = 3;
+                };
+                emoji = {
+                  name = "Emoji";
+                  module = "blink-emoji";
+                  score_offset = 1;
+                };
+                ripgrep = {
+                  name = "Ripgrep";
+                  module = "blink-ripgrep";
+                  async = true;
+                  score_offset = 1;
+                };
+                spell = {
+                  name = "Spell";
+                  module = "blink-cmp-spell";
+                  score_offset = 1;
+                };
+                calc = {
+                  name = "calc";
+                  module = "blink.compat.source";
+                  score_offset = 2;
+                };
+                npm = {
+                  name = "npm";
+                  module = "blink.compat.source";
+                  score_offset = -3;
+                };
+              }
+              // lib.optionalAttrs config.plugins.codeium-nvim.enable {
+                codeium = {
+                  name = "codeium";
+                  module = "blink.compat.source";
+                  score_offset = 100;
+                };
               };
-              codeium = {
-                name = "codeium";
-                module = "blink.compat.source";
-                score_offset = 3;
-              };
-              # copilot = {
-              #   name = "copilot";
-              #   module = "blink-cmp-copilot";
-              #   score_offset = 5;
-              # };
-              emoji = {
-                name = "emoji";
-                module = "blink.compat.source";
-                score_offset = 1;
-              };
-              git = {
-                name = "git";
-                module = "blink.compat.source";
-                score_offset = 0;
-              };
-              lsp.score_offset = 4;
-              npm = {
-                name = "npm";
-                module = "blink.compat.source";
-                score_offset = -3;
-              };
-              spell = {
-                name = "spell";
-                module = "blink.compat.source";
-                score_offset = -1;
-              };
-              #treesitter = {
-              #    name = "treesitter";
-              #    module = "blink.compat.source";
-              #  };
-              # zsh = {
-              #   name = "zsh";
-              #   module = "blink.compat.source";
-              #   score_offset = -3;
-              # };
-            };
           };
         };
       };
+
+      blink-cmp-dictionary.enable = true;
+      blink-cmp-spell.enable = true;
+      blink-copilot.enable = true;
+      blink-emoji.enable = true;
+      blink-ripgrep.enable = true;
+      blink-compat.enable = true;
     }
     (lib.mkIf config.plugins.blink-cmp.enable {
       cmp-calc.enable = true;
-      cmp-emoji.enable = true;
-      cmp-git.enable = true;
-      #cmp-nixpkgs_maintainers.enable = true;
-      cmp-npm.enable = true;
-      cmp-spell.enable = true;
-      cmp-treesitter.enable = true;
-      cmp-zsh.enable = true;
 
       lsp.capabilities = # Lua
         ''
-          capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
+          capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
         '';
     })
   ];
--- packages/wktlvim/plugins/neovide.nix
+++ packages/wktlvim/plugins/neovide.nix
@@ -1,7 +1,7 @@
 {
   extraConfigLuaPre = ''
     if vim.g.neovide then
-      vim.g.neovide_transparency = 0.9
+      -- vim.g.neovide_transparency = 0.9
       vim.g.neovide_refresh_rate = 60
       vim.g.neovide_hide_mouse_when_typing = true
       vim.g.neovide_no_idle = true
@@ -11,7 +11,7 @@
       vim.g.neovide_cursor_animate_in_insert_mode = true
       vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
-      vim.o.guifont = "RecMonoCasual Nerd Font,LXGW WenKai Mono:h14"
+      vim.o.guifont = "RecMonoCasual Nerd Font,LXGW WenKai Mono:h15"
 
       -- copy_to_clipboard
       vim.keymap.set('v', '<C-S-c>', '"+y', { noremap = true, silent = true })
