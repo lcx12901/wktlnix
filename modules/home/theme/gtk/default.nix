dcf4260abe6691623b97a5cb3a4db5f185e4276b
lcx12901 <wktl1991504424@gmail.com>
2025-03-11 00:54:38 +0800

    theme/gtk: tweaks

--- modules/home/theme/gtk/default.nix
+++ modules/home/theme/gtk/default.nix
@@ -142,7 +142,7 @@ in
       };
 
       gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
+        # gtk-application-prefer-dark-theme = true;
         gtk-decoration-layout = "appmenu:none";
         gtk-enable-event-sounds = 0;
         gtk-enable-input-feedback-sounds = 0;
