18ca43d32dfb354d9da5678d98049e4e857c8b31
lcx12901 <wktl1991504424@gmail.com>
2025-03-13 22:31:11 +0800

    chore: openssh.authorizedKeys tweaks

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -60,6 +60,7 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZAyn741cbW5FmNFKplhY2nMGYDDpx2aC0ZQFzNIkMB" # hiyori
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMR+SUAz22LypQObBh7mOhcIsY3sbeJ4xIbD8/Ju2UD" # kanroji
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII8Nmp08PzTfykfAz1lIsN3rNfurnYssyxGO0O3iXGnJ" # yukino
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZgmbR1xNbPCmUfIMCTI0+QwYKP0d5YrkbFEPNaWErW" # milet
       ];
     } // cfg.extraOptions;
 
