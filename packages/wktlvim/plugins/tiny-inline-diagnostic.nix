535b00fad9d726de69a0b9a31aebeaf40c454bf2
lcx12901 <wktl1991504424@gmail.com>
2025-03-31 23:40:40 +0800

    chore: use lsp diagnostic

--- packages/wktlvim/plugins/tiny-inline-diagnostic.nix
+++ packages/wktlvim/plugins/tiny-inline-diagnostic.nix
@@ -1,23 +1,23 @@
-{ pkgs, ... }:
+# { pkgs, ... }:
 {
-  extraPlugins = with pkgs; [
-    vimPlugins.tiny-inline-diagnostic-nvim
-  ];
-
-  extraConfigLuaPre = ''
-    require("tiny-inline-diagnostic").setup({
-      preset = "ghost",
-      options = {
-        throttle = 0,
-
-        multilines = {
-          enabled = true,
-          always_show = true,
-        },
-
-        show_all_diags_on_cursorline = true,
-        enable_on_insert = true,
-      },
-    })
-  '';
+  # extraPlugins = with pkgs; [
+  #   vimPlugins.tiny-inline-diagnostic-nvim
+  # ];
+  #
+  # extraConfigLuaPre = ''
+  #   require("tiny-inline-diagnostic").setup({
+  #     preset = "ghost",
+  #     options = {
+  #       throttle = 0,
+  #
+  #       multilines = {
+  #         enabled = true,
+  #         always_show = true,
+  #       },
+  #
+  #       show_all_diags_on_cursorline = true,
+  #       enable_on_insert = true,
+  #     },
+  #   })
+  # '';
 }
