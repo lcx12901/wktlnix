2b6ec3fd2b3cd29d656d86e8bfa261fe3ea3a49c
lcx12901 <wktl1991504424@gmail.com>
2025-03-31 23:41:12 +0800

    chore: add lzn-auto-require

--- packages/wktlvim/plugins/lz-n.nix
+++ packages/wktlvim/plugins/lz-n.nix
@@ -1,3 +1,10 @@
+{ pkgs, lib, ... }:
 {
+  extraPlugins = [ pkgs.vimPlugins.lzn-auto-require ];
+
+  extraConfigLuaPost = lib.mkOrder 5000 ''
+    require('lzn-auto-require').enable()
+  '';
+
   plugins.lz-n.enable = true;
 }
