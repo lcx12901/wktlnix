317a178b7806c55c8928feba97895be25cbc0013
lcx12901 <wktl1991504424@gmail.com>
2025-04-06 00:06:55 +0800

    sort inputs

--- flake.nix
+++ flake.nix
@@ -52,93 +52,79 @@
     };
 
   inputs = {
-    # NixPkgs (nixos-unstable)
+    # Core
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    # Home Manager (master)
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-
-    # Snowfall Lib
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Snowfall Flake
     snowfall-flake = {
       url = "github:snowfallorg/flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Declarative disk partitioning
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # many of the extensions in nixpkgs are significantly out-of-date
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-
-    niri = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Weekly updating nix-index database
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NixPkgs-Wayland
-    nixpkgs-wayland = {
-      url = "github:nix-community/nixpkgs-wayland";
+    # Addons
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    yazi-plugins = {
+      url = "github:yazi-rs/plugins";
+      flake = false;
+    };
 
-    nix-gaming = {
-      url = "github:fufexan/nix-gaming";
+    # Applications
+    niri = {
+      url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-
-    impermanence.url = "github:nix-community/impermanence";
-
-    nixvim.url = "github:nix-community/nixvim";
-
-    #FIXME: updated to include accents, https://github.com/catppuccin/nix/pull/343
-    catppuccin.url = "github:catppuccin/nix";
-
-    # Spicetify
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     waybar = {
       url = "github:Alexays/Waybar";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Aylur's gtk shell (ags)
     ags = {
       url = "github:Aylur/ags/v3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    #FIXME: updated to include accents, https://github.com/catppuccin/nix/pull/343
+    catppuccin.url = "github:catppuccin/nix";
+    catppuccin-vsc.url = "https://flakehub.com/f/catppuccin/vscode/*.tar.gz";
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    daeuniverse.url = "github:daeuniverse/flake.nix";
+    ghostty.url = "github:ghostty-org/ghostty";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
     # my wallpapers
     wallpapers = {
@@ -146,31 +132,13 @@
       flake = false;
     };
 
-    catppuccin-vsc.url = "https://flakehub.com/f/catppuccin/vscode/*.tar.gz";
-
+    nixvim.url = "github:nix-community/nixvim";
     blink-cmp = {
       url = "github:saghen/blink.cmp";
     };
-
     snacks-nvim = {
       url = "github:folke/snacks.nvim";
       flake = false;
     };
-
-    typescript-tools-nvim = {
-      url = "github:Parsifa1/typescript-tools.nvim";
-      flake = false;
-    };
-
-    yazi-plugins = {
-      url = "github:yazi-rs/plugins";
-      flake = false;
-    };
-
-    daeuniverse.url = "github:daeuniverse/flake.nix";
-
-    ghostty.url = "github:ghostty-org/ghostty";
-
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 }
