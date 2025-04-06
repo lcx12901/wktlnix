eae9ccf8fab850d57a939808540f2ff8b7ffe024
lcx12901 <wktl1991504424@gmail.com>
2025-04-06 19:11:16 +0800

    fonts: tweaks serif font

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  inherit (lib) types mkIf mapAttrs;
+  inherit (lib) types mkIf;
   inherit (lib.${namespace}) mkBoolOpt mkOpt;
 
   cfg = config.${namespace}.system.fonts;
@@ -30,31 +30,20 @@ in
       };
 
       packages = with pkgs; [
-        # Desktop Fonts
-        lxgw-wenkai
-        # Noto 系列字体是 Google 主导的，名字的含义是「没有豆腐」（no tofu），因为缺字时显示的方框或者方框被叫作 tofu
-        # Noto 系列字族名只支持英文，命名规则是 Noto + Sans 或 Serif + 文字名称。
-        # 其中汉字部分叫 Noto Sans/Serif CJK SC/TC/HK/JP/KR，最后一个词是地区变种。
-        noto-fonts # 大部分文字的常见样式，不包含汉字
-        noto-fonts-cjk-sans # 汉字部分
-        noto-fonts-emoji # 彩色的表情符号字体
-        noto-fonts-extra # 提供额外的字重和宽度变种
-        corefonts # MS fonts
-        b612 # high legibility
-        material-icons
+        # icon fonts
         material-design-icons
-        work-sans
-        comic-neue
-        source-sans
-        inter
-        lexend
+        font-awesome
 
-        # Emojis
-        noto-fonts-color-emoji
-        twemoji-color-font
+        # icon fonts
+        material-design-icons
+
+        # 思源系列字体是 Adobe 主导的。其中汉字部分被称为「思源黑体」和「思源宋体」，是由 Adobe + Google 共同开发的
+        source-sans # 无衬线字体，不含汉字。字族名叫 Source Sans 3 和 Source Sans Pro，以及带字重的变体，加上 Source Sans 3 VF
+        source-serif # 衬线字体，不含汉字。字族名叫 Source Code Pro，以及带字重的变体
+        source-han-sans # 思源黑体
+        source-han-serif # 思源宋体
 
         # Nerd Fonts
-        # "https://github.com/NixOS/nixpkgs/blob/nixos-unstable-small/pkgs/data/fonts/nerd-fonts/manifests/fonts.json"
         nerd-fonts.symbols-only # symbols icon only
         nerd-fonts.monaspace
         nerd-fonts.recursive-mono
@@ -62,7 +51,7 @@ in
         maple-mono.NF-CN
       ];
 
-      enableDefaultPackages = true;
+      enableDefaultPackages = false;
 
       fontconfig = {
         # allowType1 = true;
@@ -70,23 +59,36 @@ in
         antialias = true;
         hinting.enable = true;
 
-        defaultFonts =
-          let
-            common = [
-              "Maple Mono NF CN"
-              "Symbols Nerd Font"
-              "Noto Color Emoji"
-            ];
-          in
-          mapAttrs (_: fonts: fonts ++ common) {
-            serif = [ "Maple Mono NF CN" ];
-            sansSerif = [ "Maple Mono NF CN" ];
-            emoji = [ "Noto Color Emoji" ];
-            monospace = [
-              "Maple Mono NF CN"
-            ];
-          };
+        defaultFonts = {
+          serif = [
+            "Source Han Serif SC"
+            "Source Han Serif TC"
+            "Noto Color Emoji"
+          ];
+          sansSerif = [
+            "Source Han Sans SC"
+            "Source Han Sans TC"
+            "Noto Color Emoji"
+          ];
+          emoji = [ "Noto Color Emoji" ];
+          monospace = [
+            "Maple Mono NF CN"
+          ];
+        };
       };
+    };
+    services.kmscon = {
+      enable = true;
+      fonts = [
+        {
+          name = cfg.default;
+          package = pkgs.maple-mono.NF-CN;
+        }
+      ];
+      extraOptions = "--term xterm-256color";
+      extraConfig = "font-size=12";
+      # Whether to use 3D hardware acceleration to render the console.
+      hwRender = true;
     };
   };
 }
