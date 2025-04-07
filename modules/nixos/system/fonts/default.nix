1fde8a9de3c768618c6f7f0f0ef719f11427f9f2
linchengxu <linchengxu@z9yun.com>
2025-04-07 17:30:23 +0800

    fonts: missing noto-font-emji

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  inherit (lib) types mkIf;
+  inherit (lib) types mkIf mapAttrs;
   inherit (lib.${namespace}) mkBoolOpt mkOpt;
 
   cfg = config.${namespace}.system.fonts;
@@ -37,6 +37,10 @@ in
         # icon fonts
         material-design-icons
 
+        # Emojis
+        noto-fonts-color-emoji
+        noto-fonts-monochrome-emoji
+
         # 思源系列字体是 Adobe 主导的。其中汉字部分被称为「思源黑体」和「思源宋体」，是由 Adobe + Google 共同开发的
         source-sans # 无衬线字体，不含汉字。字族名叫 Source Sans 3 和 Source Sans Pro，以及带字重的变体，加上 Source Sans 3 VF
         source-serif # 衬线字体，不含汉字。字族名叫 Source Code Pro，以及带字重的变体
@@ -59,25 +63,31 @@ in
         antialias = true;
         hinting.enable = true;
 
-        defaultFonts = {
-          serif = [
-            "Maple Mono NF CN"
-            "Source Han Serif SC"
-            "Source Han Serif TC"
-            "Noto Color Emoji"
-          ];
-          sansSerif = [
-            "Maple Mono NF CN"
-            "Source Han Sans SC"
-            "Source Han Sans TC"
-            "Noto Color Emoji"
-          ];
-          emoji = [ "Noto Color Emoji" ];
-          monospace = [
-            "Maple Mono NF CN"
-            "Noto Color Emoji"
-          ];
-        };
+        defaultFonts =
+          let
+            common = [
+              "Maple Mono NF CN"
+              "Symbols Nerd Font"
+              "Noto Color Emoji"
+            ];
+          in
+          mapAttrs (_: fonts: fonts ++ common) {
+            serif = [
+              "Maple Mono NF CN"
+              "Source Han Serif SC"
+              "Source Han Serif TC"
+            ];
+            sansSerif = [
+              "Maple Mono NF CN"
+              "Source Han Sans SC"
+              "Source Han Sans TC"
+            ];
+            emoji = [ "Noto Color Emoji" ];
+            monospace = [
+              "Source Code Pro Medium"
+              "Source Han Mono"
+            ];
+          };
       };
     };
     services.kmscon = {
