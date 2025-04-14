9c1ec4b8fbafc05067863caf303975a7a38223f6
lcx12901 <wktl1991504424@gmail.com>
2025-04-15 00:12:29 +0800

    yazi: Refactoring

--- modules/home/programs/terminal/tools/yazi/default.nix
+++ modules/home/programs/terminal/tools/yazi/default.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   config,
   lib,
   pkgs,
@@ -7,9 +6,6 @@
   ...
 }:
 let
-  inherit (lib.${namespace}) mkBoolOpt;
-  inherit (inputs) yazi-plugins;
-
   completion = import ./keymap/completion.nix { };
   help = import ./keymap/help.nix { };
   manager = import ./keymap/manager.nix { };
@@ -18,29 +14,31 @@ let
 in
 {
   options.${namespace}.programs.terminal.tools.yazi = {
-    enable = mkBoolOpt false "Whether or not to enable yazi.";
+    enable = lib.mkEnableOption "Whether or not to enable yazi.";
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      miller
-      ouch
-      config.programs.ripgrep.package
-      xdragon
-      zoxide
-      glow
-    ];
+    home.packages =
+      let
+        optionalPluginPackage =
+          plugin: package: lib.optional (builtins.hasAttr plugin config.programs.yazi.plugins) package;
+      in
+      optionalPluginPackage "ouch" pkgs.ouch
+      ++ optionalPluginPackage "glow" pkgs.glow
+      ++ optionalPluginPackage "duckdb" pkgs.duckdb
+      ++ [
+        pkgs.xdragon
+      ];
 
     programs.yazi = {
       enable = true;
 
-      # NOTE: wrapper alias is yy
       enableBashIntegration = true;
       enableFishIntegration = true;
       enableNushellIntegration = true;
       enableZshIntegration = true;
 
-      initLua = ./configs/init.lua;
+      inherit (import ./init.nix { inherit config lib; }) initLua;
 
       keymap = lib.mkMerge [
         completion
@@ -49,19 +47,96 @@ in
       ];
 
       plugins = {
-        "chmod" = "${yazi-plugins}/chmod.yazi";
-        "diff" = "${yazi-plugins}/diff.yazi";
-        "full-border" = "${yazi-plugins}/full-border.yazi";
-        "glow" = pkgs._sources.glow-yazi.src;
-        "jump-to-char" = "${yazi-plugins}/jump-to-char.yazi";
-        "max-preview" = "${yazi-plugins}/max-preview.yazi";
-        "miller" = pkgs._sources.miller-yazi.src;
-        "ouch" = pkgs._sources.ouch-yazi.src;
-        "smart-enter" = "${yazi-plugins}/smart-enter.yazi";
-        "smart-filter" = "${yazi-plugins}/smart-filter.yazi";
+        inherit (pkgs.yaziPlugins)
+          chmod
+          diff
+          duckdb
+          full-border
+          glow
+          jump-to-char
+          mime-ext
+          mount
+          ouch
+          restore
+          smart-enter
+          smart-filter
+          sudo
+          toggle-pane
+          ;
       };
 
-      settings = import ./yazi.nix { inherit config lib pkgs; };
+      settings = lib.mkMerge [
+        (import ./settings/input.nix)
+        (import ./settings/open.nix)
+        (import ./settings/opener.nix { inherit config lib pkgs; })
+        (import ./settings/plugin.nix { inherit config lib; })
+        {
+          log = {
+            enabled = false;
+          };
+
+          manager = {
+            ratio = [
+              1
+              3
+              4
+            ];
+            linemode = "custom";
+            show_hidden = true;
+            show_symlink = true;
+            sort_by = "alphabetical";
+            sort_dir_first = true;
+            sort_reverse = false;
+            sort_sensitive = false;
+          };
+
+          pick = {
+            open_title = "Open with:";
+            open_origin = "hovered";
+            open_offset = [
+              0
+              1
+              50
+              7
+            ];
+          };
+
+          preview = {
+            tab_size = 2;
+            max_width = 600;
+            max_height = 900;
+            image_filter = "triangle";
+            image_quality = 75;
+            sixel_fraction = 15;
+            ueberzug_scale = 1;
+            ueberzug_offset = [
+              0
+              0
+              0
+              0
+            ];
+            wrap = "yes";
+          };
+
+          tasks = {
+            micro_workers = 10;
+            macro_workers = 25;
+            bizarre_retry = 5;
+            image_alloc = 536870912; # 512MB
+            image_bound = [
+              0
+              0
+            ];
+            suppress_preload = false;
+          };
+
+          which = {
+            sort_by = "none";
+            sort_sensitive = false;
+            sort_reverse = false;
+          };
+        }
+      ];
     };
   };
 }
---
+++ modules/home/programs/terminal/tools/yazi/init.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+}:
+let
+  enabledPlugins = config.programs.yazi.plugins;
+in
+{
+  initLua =
+    lib.optionalString (lib.hasAttr "full-border" enabledPlugins) ''
+      require("full-border"):setup()
+    ''
+    + lib.optionalString (lib.hasAttr "duckdb" enabledPlugins) ''
+      require("duckdb"):setup()
+    ''
+    # Lua
+    + ''
+      -- Cross session yank
+      require("session"):setup({
+        sync_yanked = true,
+      })
+
+      function Linemode:custom()
+        local year = os.date("%Y")
+        local time = (self._file.cha.mtime or 0) // 1
+
+        if time > 0 and os.date("%Y", time) == year then
+          time = os.date("%b %d %H:%M", time)
+        else
+          time = time and os.date("%b %d  %Y", time) or ""
+        end
+
+        local size = self._file:size()
+        return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
+      end
+    '';
+}
