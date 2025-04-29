a8bd15d9c1a7b8e97e8dd68c29102ed929da3bcd
lcx12901 <wktl1991504424@gmail.com>
2025-04-29 22:22:34 +0800

    service/dae: proxy steam client-update

--- modules/nixos/services/dae/default.nix
+++ modules/nixos/services/dae/default.nix
@@ -79,7 +79,7 @@ in
 
         domain(suffix: ota.waydro.id, suffix: sourceforge.net) -> proxy
         domain(suffix:duckduckgo.com) -> proxy
-        domain(suffix: steamcommunity.com, suffix: steampowered.com, suffix: fastly.steamstatic.com) -> proxy
+        domain(suffix: steamcommunity.com, suffix: steampowered.com, suffix: fastly.steamstatic.com, suffix: client-update.steamstatic.com) -> proxy
         domain(suffix: warframe.com) -> proxy
 
         dip(geoip:cn, geoip:private) -> direct
