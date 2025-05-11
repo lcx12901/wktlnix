f9c1d987e685774785940e4706e75f4c849287ce
lcx12901 <wktl1991504424@gmail.com>
2025-05-12 00:20:35 +0800

    gamemode: tweaks

--- modules/nixos/programs/graphical/games/gamemode/default.nix
+++ modules/nixos/programs/graphical/games/gamemode/default.nix
@@ -2,6 +2,7 @@
   config,
   inputs,
   lib,
+  pkgs,
   namespace,
   ...
 }:
@@ -11,26 +12,14 @@ let
 
   cfg = config.${namespace}.programs.graphical.games.gamemode;
 
-  # programs = makeBinPath [
-  #   inputs.hyprland.packages.${pkgs.stdenv.system}.default
-  #   pkgs.coreutils
-  #   pkgs.power-profiles-daemon
-  # ];
+  defaultStartScript = ''
+    ${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode started'
+  '';
 
+  defaultEndScript = ''
+    ${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode ended'
+  '';
 in
-# startscript = pkgs.writeShellScript "gamemode-start" ''
-#   export PATH=$PATH:${programs}
-#   export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
-#   hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; keyword misc:vfr 0'
-#   powerprofilesctl set performance
-# '';
-#
-# endscript = pkgs.writeShellScript "gamemode-end" ''
-#   export PATH=$PATH:${programs}
-#   export HYPRLAND_INSTANCE_SIGNATURE=$(ls -1 /tmp/hypr | tail -1)
-#   hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; keyword misc:vfr 1'
-#   powerprofilesctl set power-saver
-# '';
 {
   options.${namespace}.programs.graphical.games.gamemode = {
     enable = mkBoolOpt false "Whether or not to enable gamemode.";
@@ -41,28 +30,46 @@ in
     platformOptimizations
   ];
 
-  config = mkIf cfg.enable {
-    programs.gamemode = {
-      enable = true;
-      settings = {
-        general = {
-          softrealtime = "auto";
-          renice = 15;
+  config =
+    let
+      startScript = pkgs.writeShellScript "gamemode-start" defaultStartScript;
+      endScript = pkgs.writeShellScript "gamemode-end" defaultEndScript;
+    in
+    mkIf cfg.enable {
+      programs.gamemode = {
+        enable = true;
+        settings = {
+          general = {
+            softrealtime = "auto";
+            renice = 15;
+          };
+          # gpu = {
+          #   apply_gpu_optimisations = "accept-responsibility";
+          #   gpu_device = 0;
+          #   amd_performance_level = "high";
+          # };
+          custom = {
+            start = startScript.outPath;
+            end = endScript.outPath;
+          };
         };
-        gpu = {
-          apply_gpu_optimisations = "accept-responsibility";
-          gpu_device = 0;
-          amd_performance_level = "high";
-        };
-        # custom = {
-        #   start = startscript.outPath;
-        #   end = endscript.outPath;
-        # };
       };
-    };
 
-    # see https://github.com/fufexan/nix-gaming/#pipewire-low-latency
-    services.pipewire.lowLatency.enable = true;
-    programs.steam.platformOptimizations.enable = true;
-  };
+      security.wrappers.gamemode = {
+        owner = "root";
+        group = "root";
+        source = "${lib.getExe' pkgs.gamemode "gamemoderun"}";
+        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+      };
+
+      boot.kernel.sysctl = {
+        # default on some gaming (SteamOS) and desktop (Fedora) distributions
+        # might help with gaming performance
+        "vm.max_map_count" = 2147483642;
+      };
+
+      # see https://github.com/fufexan/nix-gaming/#pipewire-low-latency
+      # services.pipewire.lowLatency.enable = true;
+      # programs.steam.platformOptimizations.enable = true;
+    };
 }
