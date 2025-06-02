dc58a54ea6d951c0666deb98338c0015aa076ca9
lcx12901 <wktl1991504424@gmail.com>
2025-06-02 18:38:26 +0800

    chore

--- modules/home/programs/graphical/browsers/chromium/default.nix
+++ modules/home/programs/graphical/browsers/chromium/default.nix
@@ -19,19 +19,6 @@ in
 
     programs.chromium = {
       enable = true;
-
-      # extensions = with pkgs.chromium-extensions; [
-      #   catppuccin.catppuccin-vsc
-      #   eamodio.gitlens
-      #   formulahendry.auto-close-tag
-      #   formulahendry.auto-rename-tag
-      #   github.chromium-github-actions
-      #   github.chromium-pull-request-github
-      #   gruntfuggly.todo-tree
-      #   mkhl.direnv
-      #   chromium-icons-team.vscode-icons
-      #   wakatime.chromium-wakatime
-      # ];
     };
   };
 }
