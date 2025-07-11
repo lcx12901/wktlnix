3bf3551c0722e67b58167fd60fee5fcc2d79b887
lcx12901 <wktl1991504424@gmail.com>
2025-07-11 22:54:30 +0800

    mako: use service modules && use stylix theme

--- modules/home/programs/graphical/addons/mako/default.nix
+++ modules/home/programs/graphical/addons/mako/default.nix
@@ -6,61 +6,25 @@
   ...
 }:
 let
-  inherit (lib) mkIf getExe getExe';
-  inherit (lib.${namespace}) mkBoolOpt;
-
   cfg = config.${namespace}.programs.graphical.addons.mako;
 in
 {
   options.${namespace}.programs.graphical.addons.mako = {
-    enable = mkBoolOpt false "Whether to enable Mako in Hyprland.";
+    enable = lib.mkEnableOption "Whether to enable Mako in Wayland.";
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
+    stylix.targets.mako.enable = true;
+
     home.packages = with pkgs; [
-      mako
       libnotify
     ];
 
-    xdg.configFile."mako/config".source = ./config;
-
-    systemd.user.services.mako = {
-      Unit = {
-        Description = "Mako notification daemon";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-        Documentation = "man:mako(1)";
-      };
-
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-
-      Service = {
-        Type = "dbus";
-        BusName = "org.freedesktop.Notifications";
-
-        ExecCondition =
-          # bash
-          ''
-            ${getExe pkgs.bash} -c '[ -n "$WAYLAND_DISPLAY" ]'
-          '';
-
-        ExecStart =
-          # bash
-          ''
-            ${getExe pkgs.mako}
-          '';
-
-        ExecReload =
-          # bash
-          ''
-            ${getExe' pkgs.mako "makoctl"} reload
-          '';
-
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
+    services.mako = {
+      enable = true;
+      settings = {
+        border-size = 4;
+        border-radius = 8;
       };
     };
   };
