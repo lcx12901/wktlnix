efb7fb56cb9cc7224683be80039c5a16cfa0fbd2
lcx12901 <wktl1991504424@gmail.com>
2025-07-23 09:06:21 +0800

    lib: remove unuse

--- lib/modules/default.nix
+++ lib/modules/default.nix
@@ -18,22 +18,4 @@ rec {
   # return an int (1/0) based on boolean value
   # `boolToNum true` -> 1
   boolToNum = bool: if bool then 1 else 0;
-
-  default-attrs = mapAttrs (_key: mkDefault);
-
-  nested-default-attrs = mapAttrs (_key: default-attrs);
-
-  compileSCSS =
-    pkgs:
-    {
-      name,
-      source,
-      args ? "-t expanded",
-    }:
-    "${
-      pkgs.runCommandLocal name { } ''
-        mkdir -p $out
-        ${lib.getExe pkgs.sassc} ${args} '${source}' > $out/${name}.css
-      ''
-    }/${name}.css";
 }
