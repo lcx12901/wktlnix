fe7361d4d4d0bd29718de87a475535774bdf7c26
lcx12901 <wktl1991504424@gmail.com>
2025-07-30 22:14:16 +0800

    hiyori: use ddns module
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/nixos/services/inadyn/default.nix
+++ modules/nixos/services/inadyn/default.nix
@@ -10,12 +10,14 @@ in
 {
   options.${namespace}.services.inadyn = {
     enable = lib.${namespace}.mkBoolOpt false "Whether or not to configure inadyn for ddns.";
+    configFile = lib.${namespace}.mkOpt lib.types.path "" "path to singularity config";
   };
   # check ipv6 address: https://dns64.cloudflare-dns.com/cdn-cgi/trace
   config = lib.mkIf cfg.enable {
     services.inadyn = {
+      inherit (cfg) configFile;
+
       enable = true;
-      # configFile = config.age.secrets."cf-nagisa-inadyn.conf".path;
     };
   };
 }
