bccdcf784c070cf396d15067d9b64578bb14249f
lcx12901 <wktl1991504424@gmail.com>
2025-08-08 00:29:52 +0800

    nvf/vue_ls: set semanticTokens
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/home/programs/terminal/editors/nvf/lsp/vue-ls.nix
+++ modules/home/programs/terminal/editors/nvf/lsp/vue-ls.nix
@@ -33,15 +33,22 @@ in
           function(client, bufnr)
             attach_keymaps(client, bufnr);
             client.server_capabilities.documentFormattingProvider = nil
+            client.server_capabilities.semanticTokensProvider.full = true
           end
         '';
 
         on_init = mkLuaInline ''
           function(client)
             client.handlers['tsserver/request'] = function(_, result, context)
-              local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+              local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'ts_ls' })
+              local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+              local clients = {}
+
+              vim.list_extend(clients, ts_clients)
+              vim.list_extend(clients, vtsls_clients)
+
               if #clients == 0 then
-                vim.notify('Could not find `vtsls` lsp client, required by `vue_ls`.', vim.log.levels.ERROR)
+                vim.notify('Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
                 return
               end
               local ts_client = clients[1]
@@ -56,10 +63,14 @@ in
                   payload,
                 },
               }, { bufnr = context.bufnr }, function(_, r)
-                local response_data = { { id, r and r.body } }
-                ---@diagnostic disable-next-line: param-type-mismatch
-                client:notify('tsserver/response', response_data)
-              end)
+                  local response = r and r.body
+                  -- TODO: handle error or response nil here, e.g. logging
+                  -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+                  local response_data = { { id, response } }
+
+                  ---@diagnostic disable-next-line: param-type-mismatch
+                  client:notify('tsserver/response', response_data)
+                end)
             end
           end
         '';
@@ -68,16 +79,6 @@ in
           "${lib.getExe pkgs.vue-language-server}"
           "--stdio"
         ];
-
-        settings = {
-          vue.inlayHints = {
-            destructuredProps.enabled = true;
-            inlineHandlerLoading.enabled = true;
-            missingProps.enabled = true;
-            optionsWrapper.enabled = true;
-            vBindShorthand.enabled = true;
-          };
-        };
       };
     };
   };
