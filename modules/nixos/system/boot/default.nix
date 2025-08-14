2dc6a5f4d48bdf6381186ee3b138fcfcbe29956a
lcx12901 <wktl1991504424@gmail.com>
2025-08-14 23:35:27 +0800

    update flake.lock
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   namespace,
   ...
 }:
@@ -14,7 +13,6 @@ in
 {
   options.${namespace}.system.boot = {
     enable = mkBoolOpt false "Whether or not to enable booting.";
-    plymouth = mkBoolOpt false "Whether or not to enable plymouth boot splash.";
     silentBoot = mkBoolOpt false "Whether or not to enable silent boot.";
     useOSProber = mkBoolOpt false "Whether or not to append entries for other OSs detected by os-prober.";
   };
@@ -23,32 +21,27 @@ in
     boot = {
       initrd.systemd.network.wait-online.enable = false;
 
-      kernelParams =
-        lib.optionals cfg.plymouth [
-          "quiet"
-          "splash"
-        ]
-        ++ lib.optionals cfg.silentBoot [
-          # tell the kernel to not be verbose
-          "quiet"
+      kernelParams = lib.optionals cfg.silentBoot [
+        # tell the kernel to not be verbose
+        "quiet"
 
-          # kernel log message level
-          "loglevel=3" # 1: system is unusable | 3: error condition | 7: very verbose
+        # kernel log message level
+        "loglevel=3" # 1: system is unusable | 3: error condition | 7: very verbose
 
-          # udev log message level
-          "udev.log_level=3"
+        # udev log message level
+        "udev.log_level=3"
 
-          # lower the udev log level to show only errors or worse
-          "rd.udev.log_level=3"
+        # lower the udev log level to show only errors or worse
+        "rd.udev.log_level=3"
 
-          # disable systemd status messages
-          # rd prefix means systemd-udev will be used instead of initrd
-          "systemd.show_status=auto"
-          "rd.systemd.show_status=auto"
+        # disable systemd status messages
+        # rd prefix means systemd-udev will be used instead of initrd
+        "systemd.show_status=auto"
+        "rd.systemd.show_status=auto"
 
-          # disable the cursor in vt to get a black screen during intermissions
-          "vt.global_cursor_default=0"
-        ];
+        # disable the cursor in vt to get a black screen during intermissions
+        "vt.global_cursor_default=0"
+      ];
 
       kernel.sysctl = {
         "kernel.sysrq" = 1;
@@ -64,26 +57,14 @@ in
         generationsDir.copyKernels = true;
 
         grub = {
+          inherit (cfg) useOSProber;
+
           enable = true;
           default = "saved";
           devices = [ "nodev" ];
           efiSupport = true;
           efiInstallAsRemovable = true;
-          useOSProber = cfg.useOSProber;
         };
-      };
-
-      plymouth = {
-        enable = cfg.plymouth;
-        theme = "sphere";
-        themePackages = [
-          (pkgs.adi1090x-plymouth-themes.override {
-            selected_themes = [
-              "pixels"
-              "sphere"
-            ];
-          })
-        ];
       };
 
       supportedFilesystems = [
