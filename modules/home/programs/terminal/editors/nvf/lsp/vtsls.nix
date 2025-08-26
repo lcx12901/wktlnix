d077e4af4170cafe033e05cb159d955bcdc00dda
lcx12901 <wktl1991504424@gmail.com>
2025-08-27 00:56:46 +0800

    nvf/vtsls: tsserver max memory to 8G
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/home/programs/terminal/editors/nvf/lsp/vtsls.nix
+++ modules/home/programs/terminal/editors/nvf/lsp/vtsls.nix
@@ -57,18 +57,18 @@ in
         ];
 
         settings = {
-          # typescript = {
-          #   updateImportsOnFileMove.enabled = "always";
-          #   inlayHints = {
-          #     parameterNames.enabled = "all";
-          #     parameterTypes.enabled = true;
-          #     variableTypes.enabled = true;
-          #     propertyDeclarationTypes.enabled = true;
-          #     functionLikeReturnTypes.enabled = true;
-          #     enumMemberValues.enabled = true;
-          #   };
-          #   tsserver.maxTsServerMemory = 8192;
-          # };
+          typescript = {
+            updateImportsOnFileMove.enabled = "always";
+            #   inlayHints = {
+            #     parameterNames.enabled = "all";
+            #     parameterTypes.enabled = true;
+            #     variableTypes.enabled = true;
+            #     propertyDeclarationTypes.enabled = true;
+            #     functionLikeReturnTypes.enabled = true;
+            #     enumMemberValues.enabled = true;
+            #   };
+            tsserver.maxTsServerMemory = 8192;
+          };
           # javascript = {
           #   updateImportsOnFileMove.enabled = "always";
           #   inlayHints = {
