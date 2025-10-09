2044f7421e122adebc01822d0a716781576ad2f5
lcx12901 <wktl1991504424@gmail.com>
2025-10-10 01:16:42 +0800

    fastfetch: add logo
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/home/programs/terminal/tools/fastfetch/default.nix
+++ modules/home/programs/terminal/tools/fastfetch/default.nix
@@ -20,6 +20,14 @@ in
 
       settings = {
         "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+        logo = {
+          type = "kitty";
+          source = ./nixos_kawaii.png;
+          height = 16;
+          padding = {
+            top = 2;
+          };
+        };
         display = {
           color = {
             keys = "green";
