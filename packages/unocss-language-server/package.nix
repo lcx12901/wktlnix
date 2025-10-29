adb6c123fd7b4e73865e60ac784db0d00958a8fb
linchengxu <linchengxu@z9yun.com>
2025-10-29 18:36:50 +0800

    package/unocss-language-server: update version
    Signed-off-by: linchengxu <linchengxu@z9yun.com>

--- packages/unocss-language-server/package.nix
+++ packages/unocss-language-server/package.nix
@@ -1,32 +1,63 @@
 {
   lib,
-  buildNpmPackage,
-  fetchurl,
+  stdenv,
+  fetchFromGitHub,
+  pnpm,
+  nodejs,
+  nix-update-script,
+  makeBinaryWrapper,
   ...
 }:
-buildNpmPackage rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "unocss-language-server";
+  version = "0.1.8";
 
-  version = "0.1.7";
-
-  src = fetchurl {
-    url = "https://registry.npmjs.org/unocss-language-server/-/unocss-language-server-${version}.tgz";
-    sha256 = "sha256-r3pxqhbe2BxGNQCjbC65uhM8EVq9aB+9tOgiEvz52T4=";
+  src = fetchFromGitHub {
+    owner = "xna00";
+    repo = "unocss-language-server";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-rRi9JvjljvjBbY6UsH2YzAQcp+Z+MqxK7hhDNkpEANw=";
   };
 
-  npmDepsHash = "sha256-Jalcf+qc6okhGNQHWojqLof4rEimUKTShU9xIn1LNZk=";
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    fetcherVersion = 1;
+    hash = "sha256-LYsalGuxSHWiEKg3saYhREkSwEr2UaWE71V0qo8Xjzc=";
+  };
 
-  postPatch = ''
-    ln -s ${./package-lock.json} package-lock.json
+  nativeBuildInputs = [
+    nodejs
+    pnpm.configHook
+    makeBinaryWrapper
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    pnpm run build
+    runHook postBuild
   '';
 
-  dontNpmBuild = true;
+  preInstall = ''
+    rm -rf node_modules/
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/{bin,lib/unocss-language-server}
+    cp -r {bin,out} $out/lib/unocss-language-server/
+    makeWrapper ${lib.getExe nodejs} $out/bin/unocss-language-server \
+          --inherit-argv0 \
+          --add-flags $out/lib/unocss-language-server/bin/index.js
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "A language server for unocss";
     homepage = "https://github.com/xna00/unocss-language-server#readme";
-    changelog = "https://github.com/vuejs/language-tools/releases/tag/v${version}";
+    changelog = "https://github.com/vuejs/language-tools/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     mainProgram = "unocss-language-server";
   };
-}
+})
