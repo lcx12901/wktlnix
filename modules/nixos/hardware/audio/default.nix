22eb96f45156a35d2c6b1e3f953da2ad7dc2988e
lcx12901 <wktl1991504424@gmail.com>
2025-12-22 00:52:38 +0800

    audio: remove extraPackage
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -6,27 +6,19 @@
 }:
 let
   inherit (lib) mkIf mkEnableOption mkForce;
-  inherit (lib.types) listOf package;
-  inherit (lib.wktlnix) mkOpt;
 
   cfg = config.wktlnix.hardware.audio;
 in
 {
   options.wktlnix.hardware.audio = {
     enable = mkEnableOption "Whether or not to enable audio support.";
-    extra-packages = mkOpt (listOf package) [
-      pkgs.qjackctl
-      pkgs.easyeffects
-    ] "Additional packages to install.";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages =
-      (with pkgs; [
-        pulsemixer
-        pavucontrol
-      ])
-      ++ cfg.extra-packages;
+    environment.systemPackages = with pkgs; [
+      pulsemixer
+      pavucontrol
+    ];
 
     wktlnix = {
       user.extraGroups = [ "audio" ];
@@ -38,6 +30,7 @@ in
       pipewire = {
         enable = true;
         alsa.enable = true;
+        alsa.support32Bit = true;
         audio.enable = true;
         jack.enable = true;
         pulse.enable = true;
