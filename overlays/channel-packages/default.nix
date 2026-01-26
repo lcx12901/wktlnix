54e22cd2c4936d21276a0060e44e391784d26c94
lcx12901 <wktl1991504424@gmail.com>
2026-01-27 00:27:59 +0800

    linux_zen in master
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- overlays/channel-packages/default.nix
+++ overlays/channel-packages/default.nix
@@ -5,10 +5,10 @@ let
 
   dae = inputs.daeuniverse.packages."${system}".dae-unstable;
 
-  # master = import inputs.nixpkgs-master {
-  #   inherit (final.stdenv.hostPlatform) system;
-  #   inherit (final) config;
-  # };
+  master = import inputs.nixpkgs-master {
+    inherit (final.stdenv.hostPlatform) system;
+    inherit (final) config;
+  };
   # unstable = import inputs.nixpkgs-unstable {
   #   inherit (final.stdenv.hostPlatform) system;
   #   inherit (final) config;
@@ -17,4 +17,5 @@ in
 {
   inherit dae;
   # From nixpkgs-master (fast updating / want latest always)
+  inherit (master) linuxPackages_zen;
 }
