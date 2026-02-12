a7788873e4a3164dda6607c0a545e31bb76c8ba5
lcx12901 <wktl1991504424@gmail.com>
2026-02-12 22:05:08 +0800

    hiyori: enable desktop
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- homes/x86_64-linux/wktl@hiyori/default.nix
+++ homes/x86_64-linux/wktl@hiyori/default.nix
@@ -6,16 +6,53 @@ in
   wktlnix = {
     user = enabled;
 
+    system.xdg = enabled;
+
     programs = {
+      graphical = {
+        wms.niri = {
+          enable = true;
+          extraConfig =
+            let
+              inherit (lib.kdl) node leaf flag;
+              output = arg: node "output" [ arg ];
+              leaf' = name: arg: leaf name [ arg ];
+            in
+            [
+              (output "DP-1" [
+                (leaf' "mode" "2560x1440@164.998")
+                (leaf' "scale" 1.0)
+                (flag "variable-refresh-rate")
+              ])
+            ];
+        };
+      };
       terminal = {
+        emulators.kitty = {
+          fontSize = 16;
+        };
+        editors.neovim = enabled;
+        media.go-musicfox = enabled;
         tools = {
-          eza = enabled;
+          distrobox = enabled;
+          nh = enabled;
+          opencode = enabled;
         };
       };
     };
+
+    scenes = {
+      daily = enabled;
+      business = enabled;
+      development = {
+        enable = true;
+        nodejsEnable = true;
+      };
+      music = enabled;
+    };
   };
 
-  stylix.overlays.enable = false;
+  # stylix.overlays.enable = false;
 
   home.stateVersion = "24.05";
 }
