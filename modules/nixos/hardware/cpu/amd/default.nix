d5c0bcf7aee28b96ecabbae777d4ee1abe31e1e1
lcx12901 <wktl1991504424@gmail.com>
2026-02-25 23:12:15 +0800

    amd(cpu): tweaks
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/nixos/hardware/cpu/amd/default.nix
+++ modules/nixos/hardware/cpu/amd/default.nix
@@ -16,19 +16,30 @@ in
 
   config = mkIf cfg.enable {
     boot = {
+      blacklistedKernelModules = [ "k10temp" ];
       extraModulePackages = [ config.boot.kernelPackages.zenpower ];
-
       kernelModules = [
         "kvm-amd" # amd virtualization
         "zenpower" # zenpower is for reading cpu info, i.e voltage
         "msr" # x86 CPU MSR access device
       ];
 
-      kernelParams = [ "amd_pstate=active" ];
+      kernelParams = [
+        "amd_pstate=active"
+
+        # Potential stability fixes
+        "amdgpu.sg_display=0"
+        "amdgpu.dcdebugmask=0x10"
+      ];
     };
 
     environment.systemPackages = [ pkgs.amdctl ];
 
     hardware.cpu.amd.updateMicrocode = true;
+
+    powerManagement = {
+      enable = true;
+      cpuFreqGovernor = "schedutil";
+    };
   };
 }
