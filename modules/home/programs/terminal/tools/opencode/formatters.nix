51819816ede0a5945d584b429e26042486540f29
lcx12901 <wktl1991504424@gmail.com>
2026-04-06 22:22:17 +0800

    feat(opencode): add nix formatter
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

---
+++ modules/home/programs/terminal/tools/opencode/formatters.nix
@@ -0,0 +1,16 @@
+# OpenCode formatters configuration module
+# Defines code formatters for different programming languages
+{ lib, pkgs, ... }:
+{
+  config = {
+    programs.opencode.settings.formatter = {
+      nixfmt = {
+        command = [
+          (lib.getExe pkgs.nixfmt)
+          "$FILE"
+        ];
+        extensions = [ ".nix" ];
+      };
+    };
+  };
+}
