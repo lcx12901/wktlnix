6f8496b689440448cefdab6568f246f88f0eafac
lcx12901 <wktl1991504424@gmail.com>
2026-04-09 19:23:58 +0800

    WIP: use fcitx5-rime
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- modules/home/programs/graphical/addons/fcitx5/default.nix
+++ modules/home/programs/graphical/addons/fcitx5/default.nix
@@ -15,23 +15,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.sessionVariables = {
-      GTK_IM_MODULE = lib.mkForce "";
-      SDL_IM_MODULE = "fcitx";
-    };
-
-    gtk = {
-      gtk2.extraConfig = ''
-        gtk-im-module=fcitx
-      '';
-      gtk3.extraConfig = {
-        gtk-im-module = "fcitx";
-      };
-      gtk4.extraConfig = {
-        gtk-im-module = "fcitx";
-      };
-    };
-
+    # https://github.com/ulic-youthlic/flakes/blob/5f6a71f47f555747cfe2c41b79fc5a52f4bac9bb/nixos/modules/i18n.nix#L29
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
@@ -40,12 +24,81 @@ in
           kdePackages.fcitx5-chinese-addons
           fcitx5-gtk
           libsForQt5.fcitx5-qt
-          fcitx5-lua
-          fcitx5-pinyin-zhwiki
-          fcitx5-pinyin-moegirl
+          fcitx5-rime
+          # (fcitx5-rime.override {
+          #   rimeDataPkgs = [
+          #     rime-all
+          #   ];
+          # })
         ];
 
         waylandFrontend = true;
+
+        settings = {
+          globalOptions = {
+            Hotkey = {
+              EnumerateWithTriggerKeys = true;
+              AltTriggerKeys = "";
+              EnumerateForwardKeys = "";
+              EnumerateBackwardKeys = "";
+              EnumerateSkipFirst = "False";
+              EnumerateGroupForwardKeys = "";
+              EnumerateGroupBackwardKeys = "";
+              ActivateKeys = "";
+              DeactivateKeys = "";
+              PrevPage = "";
+              NextPage = "";
+              PrevCandidate = "";
+              NextCandidate = "";
+              TogglePreedit = "";
+              ModifierOnlyKeyTimeout = 250;
+            };
+            "Hotkey/TriggerKeys" = {
+              "0" = "Control+Shift+space";
+            };
+            Behavior = {
+              ActiveByDefault = "False";
+              AllowInputMethodForPassword = "False";
+              AutoSavePeriod = 30;
+              CompactInputMethodInformation = "True";
+              CustomXkbOption = "";
+              DefaultPageSize = 7;
+              DisabledAddons = "";
+              EnabledAddons = "";
+              OverrideXkbOption = "False";
+              PreeditEnabledByDefault = "True";
+              PreloadInputMethod = "True";
+              ShareInputState = "No";
+              ShowFirstInputMethodInformation = "True";
+              ShowInputMethodInformation = "True";
+              ShowPreeditForPassword = "False";
+              resetStateWhenFocusIn = "No";
+              showInputMethodInformationWhenFocusIn = "False";
+            };
+          };
+          inputMethod = {
+            "Groups/0" = {
+              Name = "Default";
+              "Default Layout" = "us";
+              DefaultIM = "rime";
+            };
+            "Groups/0/Items/0" = {
+              Name = "keyboard-us";
+              Layout = "";
+            };
+            "Groups/0/Items/1" = {
+              Name = "rime";
+              Layout = "";
+            };
+            "Groups/0/Items/2" = {
+              Name = "keyboard-us-dvp";
+              Layout = "";
+            };
+            "GroupOrder" = {
+              "0" = "Default";
+            };
+          };
+        };
       };
     };
   };
