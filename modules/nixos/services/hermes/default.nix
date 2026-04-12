bdc3982c54b0e29771dc301d3f43c2e77ec5d1e2
lcx12901 <wktl1991504424@gmail.com>
2026-04-13 00:10:09 +0800

    feat(hermes): add hermes service module and secret wiring

---
+++ modules/nixos/services/hermes/default.nix
@@ -0,0 +1,57 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    ;
+
+  inherit (pkgs.stdenv.hostPlatform) system;
+
+  cfg = config.wktlnix.services.hermes;
+in
+{
+  options.wktlnix.services.hermes = {
+    enable = mkEnableOption "Hermes agent";
+  };
+
+  config = mkIf cfg.enable {
+    services.hermes-agent = {
+      enable = true;
+      package = inputs.hermes-agent.packages.${system}.default;
+
+      environmentFiles = [ config.sops.secrets."hermes-env".path ];
+
+      settings = {
+        model = {
+          provider = "copilot";
+          default = "gpt-5-mini";
+        };
+      };
+    };
+
+    sops.secrets =
+      let
+        sopsFile = lib.file.get-file "secrets/hermes.yaml";
+      in
+      {
+        "hermes-env" = {
+          inherit sopsFile;
+          format = "yaml";
+        };
+      };
+
+    environment.persistence."/persist" = {
+      hideMounts = true;
+
+      directories = [
+        "/var/lib/hermes"
+      ];
+    };
+  };
+}
