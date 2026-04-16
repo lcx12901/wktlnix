9b59ec5a82366d9475f389112e1d7d52f398f533
lcx12901 <wktl1991504424@gmail.com>
2026-04-16 23:54:04 +0800

    chore(inadyn): remove obsolete ipv6 trace comment
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/nixos/services/inadyn/default.nix
+++ modules/nixos/services/inadyn/default.nix
@@ -11,7 +11,7 @@ in
     enable = mkEnableOption "Whether or not to configure inadyn for ddns.";
     configFile = mkOpt path "" "path to singularity config";
   };
-  # check ipv6 address: https://dns64.cloudflare-dns.com/cdn-cgi/trace
+
   config = mkIf cfg.enable {
     services.inadyn = {
       inherit (cfg) configFile;
