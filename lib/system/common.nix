1e4165b1eaa6e5382597149e4cc156c8e284f69d
lcx12901 <wktl1991504424@gmail.com>
2026-04-28 18:35:19 +0800

    chore: update immersive-translate version
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- lib/system/common.nix
+++ lib/system/common.nix
@@ -11,7 +11,7 @@ in
       allowAliases = false;
       allowUnfreePredicate = _: true;
       permittedInsecurePackages = [
-        "immersive-translate-1.27.2"
+        "immersive-translate-1.28.3"
       ];
     };
   };
