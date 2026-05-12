4e24f5d3b741403c3fe41a755f1cf42f81cf4b44
lcx12901 <wktl1991504424@gmail.com>
2026-05-13 01:14:34 +0800

    feat(openclaw): add Multi-Search-Engine skill
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

--- homes/x86_64-linux/wktl@hiyori/openclaw/skills.nix
+++ homes/x86_64-linux/wktl@hiyori/openclaw/skills.nix
@@ -7,11 +7,23 @@ let
     rev = "109f3ff4c0ad725fff94b8ab4d882aab9728b828";
     hash = "sha256-EfRqAlXbYMhOmsC7HPebmru9RQBOeog0Ve2kvXY5BrA=";
   };
+
+  openclawMasterSkills = pkgs.fetchFromGitHub {
+    owner = "LeoYeAI";
+    repo = "openclaw-master-skills";
+    rev = "262a6243eed2cfeb62e6de72fac7d2ab50e9c40f";
+    hash = "sha256-dGNS1EV+PD6NolyFbX3bGboukT2LmTasx7/rM9jjsN8=";
+  };
 in
 [
   {
     name = "self-improving-agent";
     source = "${selfImprovingAgent}";
+    mode = "copy";
+  }
+  {
+    name = "multi-search-engine";
+    source = "${openclawMasterSkills}/skills/multi-search-engine";
     mode = "copy";
   }
 ]
