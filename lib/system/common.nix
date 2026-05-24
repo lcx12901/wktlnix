5af97b18299aecf1598df8f85ddaf193028c9c93
lcx12901 <wktl1991504424@gmail.com>
2026-05-24 21:20:10 +0800

    fix(nix): bump immersive-translate to 1.29.6

--- lib/system/common.nix
+++ lib/system/common.nix
@@ -11,7 +11,7 @@ in
       allowAliases = false;
       allowUnfreePredicate = _: true;
       permittedInsecurePackages = [
-        "immersive-translate-1.29.4"
+        "immersive-translate-1.29.6"
       ];
     };
   };
