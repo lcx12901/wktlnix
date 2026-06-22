8444839779afbe3b5ad774fd3b96fca61e686a04
lcx12901 <wktl1991504424@gmail.com>
2026-06-22 17:32:31 +0800

    feat: add emilia host
    Signed-off-by: lcx12901 <wktl1991504424@gmail.com>

---
+++ systems/x86_64-linux/emilia/default.nix
@@ -0,0 +1,79 @@
+{ lib, ... }:
+let
+  inherit (lib.wktlnix) enabled;
+in
+{
+  imports = [ ./hardware.nix ];
+
+  wktlnix = {
+    system = {
+      boot = enabled;
+      disko = {
+        enable = true;
+        device = "/dev/vda";
+      };
+      fonts = enabled;
+      locale = {
+        enable = true;
+      };
+      time = enabled;
+      persist = enabled;
+      networking = {
+        enable = true;
+        optimizeTcp = true;
+      };
+    };
+
+    hardware = {
+      btrfs = enabled;
+    };
+
+    programs = {
+      terminal.tools.nix-ld = enabled;
+    };
+
+    suites = {
+      common = enabled;
+    };
+
+    services = {
+      openssh = enabled;
+    };
+
+    security = {
+      sudo-rs = enabled;
+    };
+  };
+
+  services.qemuGuest.enable = true;
+
+  networking = {
+    interfaces."eth0" = {
+      ipv4.addresses = [
+        {
+          address = "151.241.128.81";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    defaultGateway = {
+      address = "151.241.128.254";
+      interface = "eth0";
+    };
+
+    nameservers = lib.mkForce [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+}
