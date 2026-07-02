5ea017a7a01a44af20db7c5513325a8eaa0b63ae
lcx12901 <wktl1991504424@gmail.com>
2026-07-02 23:38:40 +0800

    feat(neovim): add neovim module with wktlvim integration
    Add Home Manager module for neovim using wktlvim configuration.
    Includes wakatime-cli, nvrh, and neovide in PATH.

---
+++ modules/home/programs/terminal/editors/neovim/default.nix
@@ -0,0 +1,58 @@
+{
+  inputs,
+  osConfig,
+  config,
+  system,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  wktlvimConfiguration = inputs.wktlvim.nixvimConfigurations.${system}.wktlvim;
+  wktlvim = wktlvimConfiguration.config.build.package;
+
+  persist = osConfig.wktlnix.system.persist.enable;
+
+  cfg = config.wktlnix.programs.terminal.editors.neovim;
+in
+{
+  options.wktlnix.programs.terminal.editors.neovim = {
+    enable = lib.mkEnableOption "neovim";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home = {
+      sessionVariables = {
+        EDITOR = "nvim";
+        MANPAGER = "nvim -c 'set ft=man bt=nowrite noswapfile nobk shada=\\\"NONE\\\" ro noma' +Man! -o -";
+      };
+
+      packages = [
+        wktlvim
+        pkgs.wakatime-cli
+        pkgs.nvrh
+        pkgs.neovide
+      ];
+
+      # persistence = lib.mkIf persist {
+      #   "/persist" = {
+      #     directories = [
+      #       ".local/share/nvim"
+      #       ".local/state/nvim"
+      #       ".local/cache/nvim"
+      #     ];
+      #   };
+      # };
+    };
+
+    sops.secrets =
+      let
+        dir = config.home.homeDirectory;
+      in
+      {
+        wakatime = {
+          path = "${dir}/.wakatime.cfg";
+        };
+      };
+  };
+}
